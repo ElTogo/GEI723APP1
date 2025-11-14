45af6bb071beb4f68b0086e4562d47b1655ef45c
Raphthehappy <lher0801@usherbrooke.ca>
2025-11-13 22:36:22 -0500

    readme

---
+++ Probleme2/README.md
@@ -0,0 +1,9 @@
+Le code est divisé en deux fichiers, le fichier "partie1.py" est la partie I du problème tandis que le fichier "neuro_2.ipynb" est les parties II à IV
+
+Pour le fichier "partie1.py", le code est adapté de l'exemple de STDP de brian2 se trouvant sur le lien suivant: https://brian2.readthedocs.io/en/latest/examples/synapses.STDP.html
+
+Ce code est divisé en quatre section, soit aI, aII, bI, bII. pour lancer les graphiques des différentes STDP, il faut changer les trois variables se trouvant à la ligne 79, soit eqs_x, on_pre_x et on_post_x.
+
+Pour la partie 2.b, la fréquence peut être changée à la ligne 11
+
+Pour le fichier "neuro_2.ipynb",
--- Probleme2/partie1.py
+++ Probleme2/partie1.py
@@ -8,7 +8,7 @@ vt = -54*mV
 vr = -60*mV
 El = -74*mV
 taue = 5*ms
-F = 15*Hz # Changer la fréquence pour 10 et 6 Hz pour la partie I.2.
+F = 6*Hz # Changer la fréquence pour 10 et 6 Hz pour la partie I.2.
 gmax = .01
 
 eqs_neurons = '''dv/dt = (ge * (Ee-v) + El - v) / taum : volt
@@ -26,8 +26,11 @@ dApost_aI = -dApre_aI * taupre_aI / taupost_aI * 1.05 * gmax
 eqs_aI = '''w : 1
             dApre/dt = -Apre / taupre_aI : 1 (event-driven)
             dApost/dt = -Apost / taupost_aI : 1 (event-driven)'''
-on_pre_aI = '''ge += w; Apre += dApre_aI; w = clip(w + Apost, 0, gmax)'''
-on_post_aI = '''Apost += dApost_aI; w = clip(w + Apre, 0, gmax)'''
+on_pre_aI = '''ge += w
+                Apre += dApre_aI
+                w = clip(w + Apost, 0, gmax)'''
+on_post_aI = '''Apost += dApost_aI
+                w = clip(w + Apre, 0, gmax)'''
 
 # aII, adaptation coneptuelle
 # slow et fast permet de faire la courbe descendante rapide et remontante lente dans le post
@@ -39,8 +42,11 @@ dA_slow = 0.005 * gmax
 eqs_aII = '''w : 1
              dA_slow/dt = -A_slow / tau_slow : 1 (event-driven)
              dA_fast/dt = -A_fast / tau_fast : 1 (event-driven)'''
-on_pre_aII = '''ge += w; A_slow += dA_slow; w = clip(w + A_fast, 0, gmax)'''
-on_post_aII = '''A_fast += dA_fast; w = clip(w - A_slow, 0, gmax)'''
+on_pre_aII = '''ge += w
+                A_slow += dA_slow
+                w = clip(w + A_fast, 0, gmax)'''
+on_post_aII = '''A_fast += dA_fast
+                w = clip(w - A_slow, 0, gmax)'''
 
 # bI, potentiation symétrique-
 tau_bI = 10*ms
@@ -48,8 +54,11 @@ dA_bI = .01 * gmax
 
 eqs_bI = '''w : 1
             dA/dt = -A / tau_bI : 1 (event-driven)'''
-on_pre_bI = '''ge += w; A += dA_bI; w = clip(w + A, 0, gmax)'''
-on_post_bI = '''A += dA_bI; w = clip(w + A, 0, gmax)'''
+on_pre_bI = '''ge += w
+                A += dA_bI
+                w = clip(w + A, 0, gmax)'''
+on_post_bI = '''A += dA_bI
+                w = clip(w + A, 0, gmax)'''
 
 # bII, Anti-Hebbian
 tau_bII = 20*ms
@@ -59,12 +68,15 @@ dApost_LTP = .01 * gmax
 eqs_bII = '''w : 1
              dApre/dt = -Apre / tau_bII : 1 (event-driven)
              dApost/dt = -Apost / tau_bII : 1 (event-driven)'''
-on_pre_bII = '''ge += w; Apre += dApre_LTD; w = clip(w + Apost, 0, gmax)'''
-on_post_bII = '''Apost += dApost_LTP; w = clip(w + Apre, 0, gmax)'''
+on_pre_bII = '''ge += w
+                Apre += dApre_LTD
+                w = clip(w + Apost, 0, gmax)'''
+on_post_bII = '''Apost += dApost_LTP
+                w = clip(w + Apre, 0, gmax)'''
 
 
 # Changer eqs_x, on_pre_x et on_post_x pour changer de STDP
-S = Synapses(poisson_input, neurons, eqs_aI, on_pre=on_pre_aI, on_post=on_post_aI)
+S = Synapses(poisson_input, neurons, eqs_aII, on_pre=on_pre_aII, on_post=on_post_aII)
 
 S.connect()
 S.w = 'rand() * gmax'
